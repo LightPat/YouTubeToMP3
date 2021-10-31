bced34683f97bc0eee59754a497608b511d1a7e2
LightPat <60397186+LightPat@users.noreply.github.com>
2021-10-31 15:17:52 -0400

    Audio Extraction + Basic GUI
    Added basic GUI functionality for filepaths and youtube video links.
    Can now extract audio from videos (downloads video and then deletes it after extraction).
    TODOs are in the code.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pytube import YouTube
+import PySimpleGUI as sg
 import os
 import ffmpeg
 
@@ -34,19 +35,57 @@ def downloadSource(link, outputPath):
     except Exception as e:
         print(e)
 
-def convertMP4ToMP3():
-    # TODO
-    pass
+def extractAudio(input_path, output_path, overwrite=True):
+    stream = ffmpeg.input(input_path)
+    audio = stream.audio
+    stream = ffmpeg.output(audio, output_path)
+    ffmpeg.run(stream, overwrite_output=overwrite)
 
 
 if __name__ == '__main__':
-    # My desktop path
-    save_path = r"C:\Users\patse\Desktop"
+    # GUI layout
+    layout = [  [
+        sg.Text("YouTube Video URL"),
+        sg.In(size=(25,1), enable_events=True, key="-URL-")
+    ],
+    [
+        sg.Text("Output Folder"),
+        sg.In(size=(25,1), enable_events=True, key="-FOLDER-"),
+        sg.FolderBrowse()
+    ],
 
+    [sg.Button('Ok'), sg.Button('Cancel')]
+]
+
+    # Initialize window object
+    window = sg.Window('Window Title', layout)
+
+    # Take user input and wait for button press before executing download/conversion
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Cancel':
+            window.close()
+            break
+        if event == 'Ok':
+            save_path = values['-FOLDER-']
+            vidURL = values['-URL-']
+
+            # TODO check link validity
+            if os.path.isdir(save_path):
+                # Downloads video from the internet
+                file_extension, file_name = downloadSource(vidURL, save_path)
+                # Path where the video is downloaded to
+                file_path = os.path.join(save_path, file_name) + file_extension
+                # Extract audio from video
+                extractAudio(file_path, os.path.join(save_path, "output.mp3"), overwrite=True)
+                # Remove downloaded video so that only audio is leftover
+                os.remove(file_path)
+                break
+            else:
+                # TODO Add error message to GUI
+                print("Path isn't valid")
+                continue
+
+    # EXAMPLE VIDEOS/LINKS
     # Japanese goblin https://www.youtube.com/watch?v=_bhxZD3Ravo
     # Felt https://www.youtube.com/watch?v=NLzWl57IdDI
-
-    file_extension, file_name = downloadSource("https://www.youtube.com/watch?v=NLzWl57IdDI", save_path)
-    file_path = os.path.join(save_path, file_name) + file_extension
-
-    # TODO Convert video download into audio file
